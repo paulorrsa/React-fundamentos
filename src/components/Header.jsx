8296ddd88f89e8e6c3f567272ac2671f68660f3a
paulorrsa <paulo26301@outlook.com>
2024-11-14 21:51:17 -0300

    Corrige imagem não renderizada

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+import logo from "../assets/react-core-concepts.png"
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
 function genRandonInt(max) {
@@ -8,7 +9,8 @@ function Header() {
   const description = reactDescriptions[genRandonInt(2)];
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      
+      <img src={logo} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {description} React concepts you will need for almost any app you are
